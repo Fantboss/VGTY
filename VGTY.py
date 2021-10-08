c91155d80aa4e7bc461678a7cd36deb4a1c3e94b
Fantboss <87481639+Fantboss@users.noreply.github.com>
2021-10-09 08:54:55 +0900

    server

--- VGTY.py
+++ VGTY.py
@@ -13,10 +13,13 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith("!cmd"):
-        await message.channel.send("1.!제작자")
+        await message.channel.send("1.!제작자,2.!제작자서버")
 
     if message.content.startswith("!제작자"):
         await message.channel.send("Fantboss#4687")
+
+    if message.content.startswith("!제작자서버"):
+        await message.channel.send("**https://discord.gg/QUjVvmME3E**")
 
 access_token = os.environ["BOT_TOKEN"]
 client.run("access_token")
